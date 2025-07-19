f43bdbeb658fc9f7df520f0c88524bfbd0b41b94
Maxkuji <Maxkung456@gmail.com>
2025-07-19 19:40:56 +0700

    feat: change icon to picture on nav bar

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -33,23 +33,22 @@ export default function Navbar() { // สร้างฟังก์ชัน Na
   };
 
   const navItems = [
-    { label: 'Home', href: '/', icon: '🏠' },
-    { label: 'About', href: '/about', icon: '👤' },
-    { label: 'Projects', href: '/Projects', icon: '🔲' },
-    { label: 'Blog', href: '/blog', icon: '📄' },
-    { label: 'Gallery', href: '/gallery', icon: '🖼️' },
+    { label: 'Home', href: '/', iconSrc: '/icons/home.png' },
+    { label: 'About Me', href: '/about', iconSrc: '/icons/user.png' },
+    { label: 'Projects', href: '/Projects', iconSrc: '/icons/projects.png' },
+    { label: 'Contact', href: '/Contact', iconSrc: '/icons/contact.png' },
   ];
 
   return (
     <header className="w-full flex items-center justify-between p-4 "> 
       {/* Left: Portfolio Title */}
-      <div className="text-sm font-semibold hidden md:inline">My-Portfolio</div>
+      <div className="text-lg font-bold hidden md:inline">My-Portfolio</div>
 
       {/* Center: Navbar */}
       <nav className="fixed bottom-4 left-1/2 -translate-x-1/2
       md:static md:translate-x-0 md:bottom-auto
-      flex items-center gap-2 px-4 py-2 
-      bg-white dark:bg-black border border-gray-300 dark:border-gray-700 
+      flex items-center gap px-4 py-2 
+      bg-white dark:bg-[#2C2C2C] border border-gray-300 dark:border-gray-700 
       rounded-full shadow-lg">
 
         {navItems.map((item) => (
@@ -58,14 +57,14 @@ export default function Navbar() { // สร้างฟังก์ชัน Na
             href={item.href}
             className="flex flex-col md:flex-row items-center gap-1 px-3 py-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
           >
-            <span>{item.icon}</span>
+            <img src={item.iconSrc} alt={item.label} className="w-5 h-6 md:w-7 md:h-7" />
             <span className="hidden md:inline">{item.label}</span>
           </Link>
         ))}
 
         <button
           onClick={toggleTheme}
-          className="ml-1 p-2 rounded-full border border-gray-300 dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
+          className="ml-1 p-2 rounded-full border border-gray-300 dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-500 transition-colors"
         >
           {isDark ? '☀️' : '🌙'}
         </button>
